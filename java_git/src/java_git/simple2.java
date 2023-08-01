c53febb12ef4e7fbf489b87cbae6ee764d9adb13
Mayuri <palimkarmayuri153@gmail.com>
2023-08-01 12:00:48 +0530

    commiting simple2 in branchx

---
+++ java_git/src/java_git/simple2.java
@@ -0,0 +1,17 @@
+package java_git;
+
+public class simple2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("eat something ...");
+		else {
+			System.out.println("do work....");
+		}
+	}
+
+
+}
